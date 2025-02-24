7164b4b0b39aa7e3875ca3430e1c4ee1a43a912e
Bryce Shoup <bshoup@uwyo.edu>
2025-02-24 10:41:42 -0700

    Commit from R

--- penman.R
+++ penman.R
@@ -19,8 +19,6 @@ jack.airport  <- read.csv("~/Documents/Data/Chapter.3/Weather/GSOD.Jackson.Airpo
   mutate(date = as.Date(date),
          AVGLO =  0.0864 *AVGLO) #W m-2 day-1 to MJ m-2 day-1
 
-        
-
 #Calling solar (GHI) and albedo from https://nsrdb.nrel.gov/data-viewer on a 60 minute basis
 nsrdb.solar.2022 <- read.csv("~/Documents/Data/Chapter.3/Weather/nsrdb_ghi_albedo_jackson_2022.csv", skip = 2) 
 nsrdb.solar.2023 <- read.csv("~/Documents/Data/Chapter.3/Weather/nsrdb_ghi_albedo_jackson_2023.csv", skip = 2)
@@ -60,11 +58,14 @@ amkmm <- read.csv("~/Documents/Data/Chapter.3/Weather/2024_Onset.csv", skip = 1)
   group_by(date) %>% 
   reframe(MIN = min(TEMP),
          MAX = max(TEMP),
+         RH = mean(RH),
          prcp = sum(prcp, na.rm = TRUE),
          TEMP = mean(TEMP),
          dp = mean(dp),
          WDSP = mean(WDSP),
-         ghi = mean(ghi) * 0.0864)
+         ghi = mean(ghi) * 0.0864,
+         albedo = 0.07) %>% 
+  mutate(DOY = seq(from = 172, to = 266 ))
 
 zom = 0.0002
 zoh = 0.00002
@@ -75,7 +76,47 @@ g0 = 0.3
 vonkarmaen = 0.41
 latentheatvapor = 2453
 #Want to double check the solar information but everything else looks good
-penman.parameters2 <- jack.airport %>% 
+penman.parameters.amk <- amkmm %>% 
+  mutate(
+    #aerodynamic resistance (ra) (Allen 1998)
+    ra = (log((zm - d) / zom) * log((zh - d) / zoh)) / 
+      (vonkarmaen^2 * WDSP),
+    #Atmospheric pressure (kPA) (Allen 1998)
+    pressure = 101.3 * (((293 - (0.0065 * 2064.106))/293)^5.26), 
+    #saturation vapor pressure
+    es = (.611 * exp((17.3 * TEMP)/(TEMP + 237.3))),
+    ea = (RH/100)*es,
+    #Slope of the saturation vapor pressure curve (kPa C-1) (Following Dingman, 2015)
+    delta = (2508.3 / (TEMP+237.3)^2)*exp((17.3 * TEMP)/(TEMP+237.3)),
+    #air density (allen 1998)
+    rhoa = (pressure)/(((1.01*(TEMP + 273))*0.287)),
+    #The psychometric constant (allen 1998)
+    psy = 0.000665*pressure,
+    #specific heat of air (allen 1998)
+    cp = (psy * 0.622 * 2.453)/ pressure,
+    dr = 1 + (0.033 * cos(((2 * pi)/365)*DOY)),
+    del = 0.409 * sin(((2 * pi*DOY)/365 )- 1.39),
+    latrad = (pi/180) * 43.605628,
+    sunset = acos(-tan(latrad) * tan(del)),
+    sin = sunset * sin(latrad) * sin(del),
+    cos = sin(sunset) * cos(latrad) * cos(del),
+    sincos = sin + cos,
+    ra = ((24*60)/pi) * 0.0820 * dr * sincos,
+    rso = (0.75 + ((2*10^-5)* 2064.106)) * ra,
+    rns = (1 - albedo) * ghi,
+    tmaxk =(MAX + 273.16)^4,
+    tmink = (MIN + 273.16)^4,
+    tmmk = (tmaxk+tmink)/2,
+    rnl2term = 0.34 - (0.14 * sqrt(ea)),
+    rnl3term = ((1.35* ghi)/rso) - 0.35,
+    rnl = (4.903 * 10^-9) * tmmk * rnl2term * rnl3term,
+    rn = rns - rnl,
+    ettest = 1000*(((delta * rn) + (rhoa * cp * ((es-ea) / ra)))/
+                     (latentheatvapor * (delta+ psy))),
+    ettest2 = 1000*(((delta * (rn - (rn*g0))) + (rhoa * cp * ((es-ea) / ra)))/
+                      (latentheatvapor * (delta+ psy))))
+#Want to double check the solar information but everything else looks good
+penman.parameters.gsod <- jack.airport %>% 
   mutate(
     #aerodynamic resistance (ra) (Allen 1998)
     ra = (log((zm - d) / zom) * log((zh - d) / zoh)) / 
@@ -208,41 +249,4 @@ penman.parameters1 <- jack.airport %>%
 
 
 
-
-penman.parameters <- jack.airport %>% 
-  mutate(
-  #Slope of the saturation vapor pressure curve (kPa C-1)
-  delta = ((4098 * (0.6108 * exp((17.27*TEMP) / (TEMP + 237.3)))) / ((TEMP + 237.3)^2)),
-  #Atmospheric pressure (kPA)
-  pressure = 101.3 * (((293 - (0.0065 * 2064.106))/293)^5.26), 
-  #The psychometric constant
-  psy = 0.000665 * pressure,
-  deltaterm = (delta)/(delta + (psy * (1 + (0.34 * WDSP)))),
-  psiterm = (psy)/(delta + (psy * (1 + (0.34 * WDSP)))),
-  tempterm = ((900)/(TEMP+273)) * WDSP,
-  etmax = 0.6108 * exp(((17.27 * MAX)/(MAX + 237.3))),
-  etmin = 0.6108 * exp(((17.27 * MIN)/(MIN + 237.3))),
-  es = (etmax + etmin)/2,
-  ea = (RH/100)* ((etmin + etmax)/2),
-  dr = 1 + (0.033 * cos(((2 * pi)/365)*DOY)),
-  del = 0.409 * sin(((2 * pi*DOY)/365 )- 1.39),
-  latrad = (pi/180) * 43.605628,
-  sunset = acos(-tan(latrad) * tan(del)),
-  sin = sunset * sin(latrad) * sin(del),
-  cos = sin(sunset) * cos(latrad) * cos(del),
-  sincos = sin + cos,
-  ra = ((24*60)/pi) * 0.0820 * dr * sincos,
-  rso = (0.75 + ((2*10^-5)* 2064.106)) * ra,
-  rns = (1 - albedo) * ghi,
-  tmaxk =(MAX + 273.16)^4,
-  tmink = (MIN + 273.16)^4,
-  tmmk = (tmaxk+tmink)/2,
-  rnl2term = 0.34 - (0.14 * sqrt(ea)),
-  rnl3term = ((1.35* ghi)/rso) - 0.35,
-  rnl = (4.903 * 10^-9) * tmmk * rnl2term * rnl3term,
-  rn = rns - rnl,
-  rng = 0.408 *rn,
-  etrad = deltaterm * rng,
-  etwind = psiterm * (tempterm * (es-ea)),
-  et = etrad + etwind)
 
